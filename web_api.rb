004fd1df144b4efea8987a810d74583290978bd0
Shota Nakano <shota.nakano@manycolors-inc.com>
2015-10-23 14:36:11 +0900

    check .rb scripts

--- web_api.rb
+++ web_api.rb
@@ -6,7 +6,3 @@ simple_http.http_version = "HTTP/1.1"
 # get
 puts simple_http.get("/[key]/slack/temp/[id]/20.44")
 #puts simple_http.get("/[key]/twitter/hum/[id]/50")
-
-simple_http = SimpleHttp.new("108.61.163.72", "9393")
-simple_http.http_version = "HTTP/1.1"
-simple_http.get("/590544ee05762c62719e87d4ed79f807/slack/temp/10/21")
